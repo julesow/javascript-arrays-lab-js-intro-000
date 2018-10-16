167ce8d20613b92dc26f29bc814a07a3b2a58968
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-10-16 07:15:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ function destructivelyAppendKitten(name){ kittens.push(name)}
 function destructivelyPrependKitten(name){kittens.unshift(name)}
 
 function destructivelyRemoveLastKitten(){kittens.pop()}
+
+function destructivelyRemoveFirstKitten(){kittens.shift()}
